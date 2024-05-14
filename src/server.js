74f1876e0652fd24662db92b66f70f88f4488c17
Nam Phuong <pnam24823@gmail.com>
2024-05-14 23:23:22 +0700

    connect_mongodb_env_variables

--- src/server.js
+++ src/server.js
@@ -1,27 +1,47 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello Con Cark Do</h1><hr>')
+  })
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, Back-End Server is running succesfully at Host: ${env.APP_HOST} and Port: ${env.APP_PORT}`)
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello REST-API</h1><hr>')
-})
+  exitHook(() => {
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+    console.log('5. Disconnected MongoDB Cloud Atlas!')
+  })
+}
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello NamPhuongDev, I am running at http://${ hostname }:${ port }/`)
-})
+
+// //Chỉ khi connect tới database thành công thì mới Start Back-End lên
+// console.log('1. Connecting to MongoDB CLoud Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB CLoud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+
+// Cách viết khác: IIFE in JS
+//Chỉ khi connect tới database thành công thì mới Start Back-End lên
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB CLoud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB CLoud Atlas!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
